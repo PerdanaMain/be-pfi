2098617e6bc6091131576e84b938f33165a51199
Perdana Main <firman.fp123@gmail.com>
2024-10-04 14:05:46 +0700

    update config

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ from config import Config
 
 if __name__ == "__main__":
     port = Config.PORT
-    app.run(port=port, debug=True, host="0.0.0.0")
+    app.run(
+        port=port,
+        debug=True,
+    )
