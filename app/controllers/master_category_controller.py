14ef6cf994b7321942f7c80490bf1669e7629481
Perdana Main <firman.fp123@gmail.com>
2024-10-09 14:28:45 +0700

    make equipment trees and update update also categories

--- app/controllers/master_category_controller.py
+++ app/controllers/master_category_controller.py
@@ -1,8 +1,5 @@
-from flask import jsonify, make_response, request
-from app.resources.master_category_resource import category_resource
-from digital_twin_migration.models.pfi_app import PFICategory
-from digital_twin_migration.database import Transactional, Propagation
-from app.services.response import success, created, bad_request, not_found
+from flask import request
+from app.services.response import success, created, bad_request
 from app.services.orm.master_category import (
     get_all_categories,
     create_category,
@@ -19,13 +16,17 @@ def index():
         return bad_request(False, f"Internal Server Error: {e}", None)
 
 
-@Transactional(propagation=Propagation.REQUIRED)
 def create():
     try:
         req = request.get_json()
 
         data = {
             "name": req.get("name"),
+            "rigid_or_flexy": req.get("rigid_or_flexy"),
+            "edges_a": req.get("edges_a"),
+            "edges_b": req.get("edges_b"),
+            "edges_c": req.get("edges_c"),
+            "edges_d": req.get("edges_d"),
         }
         create_category(data)
         return created(True, "Master Category created successfully", None)
@@ -33,7 +34,6 @@ def create():
         return bad_request(False, f"Internal Server Error: {e}", None)
 
 
-@Transactional(propagation=Propagation.REQUIRED)
 def delete(id):
     try:
         delete_category(id)
--- app/resources/master_category_resource.py
+++ app/resources/master_category_resource.py
@@ -2,6 +2,11 @@ def category_resource(category):
     return {
         "id": category.id,
         "name": category.name,
+        "rigid_or_flexy": category.rigid_or_flexy,
+        "edges_a": category.edges_a,
+        "edges_b": category.edges_b,
+        "edges_c": category.edges_c,
+        "edges_d": category.edges_d,
         "created_at": category.created_at,
         "updated_at": category.updated_at,
     }
