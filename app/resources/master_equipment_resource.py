88be60ef86689864724cb76c1a1f54d6b02e3806
Perdana Main <firman.fp123@gmail.com>
2024-10-28 08:38:28 +0700

    equipments: update api pagination

--- app/resources/master_equipment_resource.py
+++ app/resources/master_equipment_resource.py
@@ -36,19 +36,20 @@ def equipment_resource(equipment):
         "updated_at": equipment.updated_at,
     }
 
+
 def paginate(data, page, per_page):
     total_items = len(data)
-    total_pages = (total_items + per_page - 1) // per_page 
+    total_pages = (total_items + per_page - 1) // per_page
     start = (page - 1) * per_page
     end = start + per_page
     paginated_data = data[start:end]
-    
+
     return {
         "quipments": paginated_data,
         "pagination": {
             "total_items": total_items,
             "total_pages": total_pages,
             "current_page": page,
-            "limit": per_page
-        }
+            "limit": per_page,
+        },
     }
