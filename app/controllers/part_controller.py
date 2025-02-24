df8ab77ce3977d8ff6a8af6807bd553a373d8998
perdanamain <firman.fp123@gmail.com>
2025-02-24 13:34:16 +0700

    fix: error at part config

--- app/controllers/part_controller.py
+++ app/controllers/part_controller.py
@@ -6,11 +6,7 @@ from app.services.models.part_model import *
 def index():
     try:
         data = get_all_parts()
-        return success(
-            True,
-            "Master Equipment fetched successfully",
-            {"parts": data, "total": len(data)},
-        )
+        return success(True, "Master Equipment fetched successfully", data)
     except Exception as e:
         return bad_request(False, f"Internal Server Error: {e}", None)
 
