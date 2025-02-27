7d2b33ffb24832b7c3fe19fa8259938b884f1d22
perdanamain <firman.fp123@gmail.com>
2025-02-27 14:15:17 +0700

    refactor: chart information

--- app/controllers/chart_controller.py
+++ app/controllers/chart_controller.py
@@ -3,7 +3,7 @@ from app.services.response import *
 from app.services.models.oh_schedule_model import get_oh_schedule_by_year
 from app.services.models.feature_data_model import get_last_data_value
 from app.services.models.part_model import get_part
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 
 
@@ -20,6 +20,7 @@ def calculate_time_difference(time_failure_str, date_time_str):
     days_diff = round(days_diff)
 
     return days_diff
+
 
 def calculate_time_difference_to_hours(time_failure_str, date_time_str):
     if isinstance(time_failure_str, datetime) and isinstance(date_time_str, datetime):
@@ -41,71 +42,55 @@ def information_chart():
         features_id = request.args.get("features_id", default=1, type=str)
         part_id = request.args.get("part_id", default=1, type=str)
 
-        informations = []
         current_value = get_last_data_value(part_id=part_id, feature_id=features_id)
         part = get_part(part_id)
         now = datetime.now()
 
-        predict_time_to_failure = (
-            calculate_time_difference(
+        failure_hours = (
+            calculate_time_difference_to_hours(
                 current_value["values"][0]["time_failure"],
                 current_value["values"][0]["date_time"],
             )
             if current_value["values"][0]["predict_status"] == "predicted failed"
             else None
         )
-        
-        failure_hours = calculate_time_difference_to_hours(
-            current_value["values"][0]["time_failure"],
-            current_value["values"][0]["date_time"],
-        ) if current_value["values"][0]["predict_status"] == "predicted failed" else None
 
         oh_schedules = get_oh_schedule_by_year(year=datetime.now().year)
-        oh_start = calculate_time_difference(
-            oh_schedules["oh_schedules"]["start"],
-            now,
-        )
+
         oh_start_hours = calculate_time_difference_to_hours(
             oh_schedules["oh_schedules"]["start"],
             now,
         )
+        failure_hours = failure_hours if failure_hours is not None else oh_start_hours
 
-        informations.append(
+        # Convert failure_hours to a date
+        failure_date = now + timedelta(hours=failure_hours)
+
+        response = [
             {
                 "name": f"predicted failure interval",
-                "value": (
-                    predict_time_to_failure
-                    if predict_time_to_failure is not None
-                    else oh_start
-                ),
-                "satuan": "Days",
-            }
-        )
-        informations.append(
+                "value": failure_hours,
+                "satuan": "Hours",
+                "detail": {
+                    "date": failure_date.strftime("%A, %d %B %Y %H:%M:%S"),
+                    "hours": failure_hours,
+                },
+            },
             {
                 "name": "current condition",
                 "value": "-",
                 "satuan": "%",
             },
-        )
-        informations.append(
             {
                 "name": "current value",
                 "value": round(current_value["values"][0]["value"], 4),
                 "satuan": part["part"]["unit"],
             },
-        )
-        informations.append(
-            {
-                "name": "failure hours",
-                "value": failure_hours if failure_hours is not None else oh_start_hours,
-                "satuan": "hours",
-            },
-        )
+        ]
         return success(
             True,
             "Chart Information Fetched Successfully",
-            informations,
+            response,
         )
     except Exception as e:
         return bad_request(False, f"Internal Server Error: {e}", None)
